8f8c332695cd3b688389fd616905fe0c0ba38b76
Jamal <JamalG94>
2018-03-25 17:49:24 +0200

    Created a timetable in xml, started building the blocks for adding lessons to a time slot.

--- app/src/main/java/com/example/jamal/orderhr_noninstant/ScheduleActivity.java
+++ app/src/main/java/com/example/jamal/orderhr_noninstant/ScheduleActivity.java
@@ -3,21 +3,40 @@ package com.example.jamal.orderhr_noninstant;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CalendarView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 /**
  * Created by jamal on 3/19/2018.
  */
 
 public class ScheduleActivity extends AppCompatActivity {
-    CalendarView calendarView;
+
+    private TableLayout schedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+        schedule = findViewById(R.id.schedule_lessons);
+        FillSchedule("bullshit");
+    }
 
-        calendarView = findViewById(R.id.scheduleCalendar);
+    private void FillSchedule(String json){
+        String tablerow_id;
+        String day;
+        String lesson;
+        String teacher;
+        int timeslot;
+
+        //foreach loop to go through all the json data
+        tablerow_id = "timeslot_one";
+
+        int id = getResources().getIdentifier(tablerow_id, "id", getPackageName());
+        TableRow tableRow = (TableRow)findViewById(R.id.timeslot_one);
+        System.out.print(tableRow);
 
 
     }
+
 }
