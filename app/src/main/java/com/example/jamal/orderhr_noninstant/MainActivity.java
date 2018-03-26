73687915c4ebc481dc336e19522ce23fc52219ce
Jamal <JamalG94>
2018-03-26 17:45:09 +0200

    Dynamic lesson adding
    ScheduleActivity now adds two lessons on monday based on mockdata

--- app/src/main/java/com/example/jamal/orderhr_noninstant/MainActivity.java
+++ app/src/main/java/com/example/jamal/orderhr_noninstant/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.jamal.orderhr_noninstant;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+    }
+
+    public void ClickSchedule(View view){
+        Intent intent = new Intent(this, ScheduleActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/jamal/orderhr_noninstant/ScheduleActivity.java
+++ app/src/main/java/com/example/jamal/orderhr_noninstant/ScheduleActivity.java
@@ -2,9 +2,9 @@ package com.example.jamal.orderhr_noninstant;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.CalendarView;
-import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+
 
 /**
  * Created by jamal on 3/19/2018.
@@ -12,30 +12,36 @@ import android.widget.TableRow;
 
 public class ScheduleActivity extends AppCompatActivity {
 
-    private TableLayout schedule;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
-        schedule = findViewById(R.id.schedule_lessons);
-        FillSchedule("bullshit");
     }
 
-    private void FillSchedule(String json){
+    @Override
+    protected void onStart(){
+        super.onStart();
+        FillSchedule();
+    }
+
+    private void FillSchedule(){
         String tablerow_id;
         String day;
-        String lesson;
-        String teacher;
-        int timeslot;
+        String lesson = "ICTLAB";//booking.Lesson;
+        String teacher = "Omar";//booking.Username;
+        int timeslotfrom = 3;
+        int timeslotto = 4;
 
-        //foreach loop to go through all the json data
-        tablerow_id = "timeslot_one";
+        TextView timeslot_lesson;
+        TableRow tablerow;
 
-        int id = getResources().getIdentifier(tablerow_id, "id", getPackageName());
-        TableRow tableRow = (TableRow)findViewById(R.id.timeslot_one);
-        System.out.print(tableRow);
-
+        for(int i = timeslotfrom; i <= timeslotto; i++ ){
+            timeslot_lesson = new TextView(this);
+            tablerow_id = "timeslot_" + i;
+            tablerow = findViewById(getResources().getIdentifier(tablerow_id, "id", getPackageName()));
+            timeslot_lesson.setText(teacher + lesson);
+            tablerow.addView(timeslot_lesson);
+        }
 
     }
 
