3a9805a138ed346c0e4b6d8995461b24d5f6f446
RobinVrijmoet <0907313@hr.nl>
2018-05-23 12:10:51 +0200

    filters and details added

--- app/src/main/java/com/example/jamal/orderhr_noninstant/Activities/Defuncts/DefunctDetailActivity.java
+++ app/src/main/java/com/example/jamal/orderhr_noninstant/Activities/Defuncts/DefunctDetailActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.jamal.orderhr_noninstant.Activities.IDataStructure;
 import com.example.jamal.orderhr_noninstant.Datastructures.DefunctWrapper;
@@ -22,7 +24,8 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
+
+import static android.view.View.GONE;
 
 /**
  * Created by Robin on 3/22/2018.
@@ -34,109 +37,98 @@ public class DefunctDetailActivity extends AppCompatActivity implements IDataStr
     IO ioinstance;
     Spinner typeselectionfilter;
     Switch switchhandled;
-    public String getdatafromio(boolean handled){
-        String returnjson = "";
-        ioinstance = IO.GetInstance();
-
-            returnjson  = ioinstance.DoPostRequestToAPIServer("{\"handled\":"+handled+"}","http://markb.pythonanywhere.com/alldefuncts/",this);
-
-
-//            if(handled){
-//                returnjson = ioinstance.DoPostRequestToAPIServer()
-//            }else{
-//
-//            }
-//            if(type.equals("")){
-//                returnjson = ioinstance.DoPostRequestToAPIServer("{  \"handled\":\"False\"}","http://markb.pythonanywhere.com/alldefuncts/");
-//            }else{
-//                returnjson = ioinstance.DoPostRequestToAPIServer("{  \"type\":\""+type+"\",\"handled\":\"False\"}","http://markb.pythonanywhere.com/alldefuncts/");
-//            }
-
-
-        return returnjson;
-    }
+    DefunctViewAdapter defunctarrayadapt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_defunctlist);
-
-
-
         receiveddefuncts = new ArrayList<>();
 
+        //DUE TO LIMITS IN OUR API CALL, WE NEED TO DO 2 API CALLS, ONE TO GET ALL THE HANDLED DEFUNCTS
+        //AND ONE TO GET THE UNHANDLED DEFUNCTS.
         this.IFillDataStructures(new ObjectMapper(),getdatafromio(true));
         this.IFillDataStructures(new ObjectMapper(),getdatafromio(false));
-        screenlistview = (ListView)findViewById(R.id.listviewDefuncts);
-        typeselectionfilter = (Spinner)findViewById(R.id.spinnertypefiler);
-        switchhandled = (Switch)findViewById(R.id.switch1);
-        ModifyAllReceivedBookingsToListView(this.receiveddefuncts,typeselectionfilter.getSelectedItem().toString(),switchhandled.isChecked());
-//        TextView roomview = (TextView)findViewById(R.id.viewroomid);
-//        roomview.setText(roomview.getText() + "WN012");
 
+        loadMainDefunctListToView(findViewById(android.R.id.content));
     }
 
-    public void ModifyAllReceivedBookingsToListView(List<DefunctWrapper> receiveddefuncts, String type,boolean showhandled){
-        ArrayAdapter<String> test = (new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1));
-        screenlistview.setAdapter(test);
-        for (DefunctWrapper receiveddefunct: receiveddefuncts) {
+    public void loadMainDefunctListToView(View view){
+        setContentView(R.layout.activity_defunctlist);
+        screenlistview = (ListView)findViewById(R.id.listviewDefuncts);
+
+        typeselectionfilter = (Spinner)findViewById(R.id.spinnertypefiler);
+        switchhandled = (Switch)findViewById(R.id.switch1);
+        ModifyAllReceivedBookingsToListView2(typeselectionfilter.getSelectedItem().toString(),switchhandled.isChecked());
+
+        screenlistview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                DefunctWrapper p = (DefunctWrapper) screenlistview.getItemAtPosition(i);
+                setContentView(R.layout.activity_defunct);
+                TextView tvid = (TextView)findViewById(R.id.textviewID);
+                TextView tvdescription = (TextView)findViewById(R.id.textviewdescription);
+                TextView tvtype = (TextView)findViewById(R.id.textviewtype);
+                TextView tvhandled = (TextView)findViewById(R.id.textviewhandled);
+                Button buttonsethandled = (Button)findViewById(R.id.buttonsetHandled);
+                buttonsethandled.setVisibility(GONE);
+                tvhandled.setText("Handled : " + p.getFields().isHandled());
+                tvid.setText(tvid.getText()+" " + p.getPk());
+                tvdescription.setText(tvdescription.getText() +" " + p.getFields().getDescription());
+                tvtype.setText(tvtype.getText() + " " +p.getFields().getType());
+            }
+        });
+    }
+
+
+    public List<DefunctWrapper> FillListWithFilteredItems(String type, final boolean showhandled){
+        List<DefunctWrapper> filtereddefunctlist = new ArrayList<>();
+        for(DefunctWrapper receiveddefunct: receiveddefuncts){
             if(!receiveddefunct.getFields().isHandled() || !showhandled){
                 if(!type.equals("view all")){
                     if(receiveddefunct.getFields().getType().toUpperCase().equals(type.toUpperCase())){
-                        test.add(receiveddefunct.getFields().getRoom() + " " + receiveddefunct.getFields().getType() + " " +  receiveddefunct.getFields().getDate());
+                        filtereddefunctlist.add(receiveddefunct);
                     }
                 }else{
-                    test.add(receiveddefunct.getFields().getRoom() + " " + receiveddefunct.getFields().getType() + " " +  receiveddefunct.getFields().getDate());
+                    filtereddefunctlist.add(receiveddefunct);
 
                 }
             }
-
-//            Button gotowidget = new Button(this);
-//            gotowidget.setText(receiveddefunct.getRoom() + receiveddefunct.getType() + receiveddefunct.getDate());
         }
-        test.notifyDataSetChanged();
+        return filtereddefunctlist;
+    }
+    //NEED TO SPLIT FUNCTIONALITIES HERE:
+    public void ModifyAllReceivedBookingsToListView2(String type, boolean showhandled){
+        List<DefunctWrapper> filtereddefunctlist = FillListWithFilteredItems(type,showhandled);
+
+        defunctarrayadapt = new DefunctViewAdapter(this,R.layout.defunct_list_view_item,filtereddefunctlist);
+        screenlistview.setAdapter(defunctarrayadapt);
+        defunctarrayadapt.notifyDataSetChanged();
     }
 
+    //Updates the shown defuncts
     public void OnClickSearch(View view){
-//        IFillDataStructures(new ObjectMapper(),getdatafromio(typeselectionfilter.getSelectedItem().toString()));
-        ModifyAllReceivedBookingsToListView(receiveddefuncts,typeselectionfilter.getSelectedItem().toString(),switchhandled.isChecked());
+        ModifyAllReceivedBookingsToListView2(typeselectionfilter.getSelectedItem().toString(),switchhandled.isChecked());
     }
 
+    //Does an API call to get data from the server
+    public String getdatafromio(boolean handled){
+        String returnjson = "";
+        ioinstance = IO.GetInstance();
+
+        returnjson  = ioinstance.DoPostRequestToAPIServer("{\"handled\":"+handled+"}","http://markb.pythonanywhere.com/alldefuncts/",this);
+        return returnjson;
+    }
 
     @Override
     public void IFillDataStructures(ObjectMapper objectMapper, String json) {
-
-        //First get json
-        //Give json and api link to io
-        //convert io data into list of defuncts
         //TODO JSONException
         try{
             JSONArray jsonstuff = new JSONArray(json);
-//            JSONArray values = jsonstuff.
-//            JSONObject values = jsonstuff.getJSONObject(1);
-//            values.equals(jsonstuff);
-
+            //Add all defucnts found in the json to the list.
             for (int i=0; i < jsonstuff.length(); i++) {
                 JSONObject jsonobjectparser = jsonstuff.getJSONObject(i);
                 receiveddefuncts.add( objectMapper.readValue(jsonobjectparser.toString(), DefunctWrapper.class));
             }
-
-//            receiveddefuncts = new ArrayList<>();
-//            Defunct defunct1 = new Defunct();
-//            defunct1.setRoom("WN.4.401");
-//            defunct1.setDefunctid(1);
-//            defunct1.setType("notype");
-//            defunct1.setDescription("I don' t know what I' m doing");
-//            defunct1.setDate(new Date(2012,1,12));
-//            Defunct defunct2= new Defunct();
-//            defunct2.setRoom("WN.1.116");
-//            defunct2.setDefunctid(2);
-//            defunct2.setType("missing");
-//            defunct2.setDate(new Date(2014,12,1));
-//            defunct2.setDescription("Went into this room to put my keys on the table, only to find out the table is missing. please fix");
-//            receiveddefuncts.add(defunct1);
-//            receiveddefuncts.add(defunct2);
-//
         }catch(JSONException|IOException e){
             Log.i("hello?",e.getMessage());
         }
