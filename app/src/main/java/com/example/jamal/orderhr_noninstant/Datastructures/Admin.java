613ec9e11b3fb1a60af2c8112b3d95ee51dee13e
Jamal <JamalG94>
2018-05-24 11:59:25 +0200

    Added type of users as classes.

---
+++ app/src/main/java/com/example/jamal/orderhr_noninstant/Datastructures/Admin.java
@@ -0,0 +1,12 @@
+package com.example.jamal.orderhr_noninstant.Datastructures;
+
+/**
+ * Created by jamal on 5/24/2018.
+ */
+
+public class Admin extends SuperUser {
+
+    public Admin(){
+        this.SetTypeUser("Admin");
+    }
+}
